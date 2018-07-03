4dfbb2a5fa64e3fe1423e6b9acacb84f369ca6da
ximenes <xresende@gmail.com>
2018-07-03 17:19:22 -0300

    Update rotcoil scripts

--- magnet/excitation-data/scripts/rotcoil.py
+++ magnet/excitation-data/scripts/rotcoil.py
@@ -3,6 +3,8 @@
 """Read rotation coil data."""
 
 import os
+import numpy as np
+
 from siriuspy import envars
 
 
@@ -90,8 +92,8 @@ class RotCoilData:
 
         # process data
         self.harmonics = list()
-        self.strength_normal_avg = list()
-        self.strength_skew_avg = list()
+        self.intmpole_normal_avg = list()
+        self.intmpole_skew_avg = list()
         n1 = lines.index('##### Reading Data #####') + 3
         n2 = lines.index('##### Raw Data Stored(V.s) [1e-12] #####') - 5
         for i in range(n1, n2):
@@ -99,8 +101,8 @@ class RotCoilData:
             n = int(words[0])
             multipoles = [float(w) for w in words[1:]]
             self.harmonics.append(n)
-            self.strength_normal_avg.append(multipoles[0])
-            self.strength_skew_avg.append(multipoles[2])
+            self.intmpole_normal_avg.append(multipoles[0])
+            self.intmpole_skew_avg.append(multipoles[2])
             # print(n, multipoles)
 
     @staticmethod
@@ -122,9 +124,69 @@ class RotCoilMeas:
         self._read_rotcoil_data()
 
     @property
-    def data_set(self):
+    def data_sets(self):
         """Return list of data set."""
-        return self._get_data_set()
+        return self._get_data_sets()
+
+    def get_data_set_measurements(self, data_set):
+        """."""
+        return self._rotcoildata[data_set]
+
+    def get_max_current_index(self):
+        """Return max current index."""
+        max_c_i = []
+        for data_set in self.data_sets:
+            # data = self._rotcoildata[data_set]
+            currents = self.get_currents(data_set)
+            max_c = max(currents)
+            max_c_i.append(currents.index(max_c))
+        umaxci = np.unique(max_c_i)
+        if len(umaxci) > 1:
+            raise ValueError('Inconsistent current values in data sets')
+        return umaxci[0]
+
+    def get_rampup(self, data_set):
+        """Rampup data."""
+        c = self.get_currents(data_set)
+        gl = self.get_intmpole_normal_avg(data_set, self.main_harmonic)
+        i_max = self.get_max_current_index()
+        return c[:i_max+1], gl[:i_max+1]
+
+    def get_rampdown_hysteresis(self, data_set):
+        """Rampdown hysteresis."""
+        c = self.get_currents(data_set)
+        gl = self.get_intmpole_normal_avg(data_set, self.main_harmonic)
+        i_max = self.get_max_current_index()
+        c_lin = c[0:i_max+1]
+        gl_lin = gl[0:i_max+1]
+        gl_int = np.interp(c, c_lin, gl_lin)
+        gl_dif = [gl_int[i] - gl[i] for i in range(len(c))]
+        c, h = c[i_max:], gl_dif[i_max:]
+        area = -np.trapz(h, c)
+        return c, h, area
+
+    def get_currents(self, data_set):
+        """Return currents of a data set."""
+        data = self._rotcoildata[data_set]
+        return [d.main_coil_current_avg for d in data]
+
+    def get_intmpole_normal_avg(self, data_set, n):
+        """Return average integrated normal multipole."""
+        i = self.harmonics.index(n)
+        data = self._rotcoildata[data_set]
+        p = []
+        for datum in data:
+            p.append(datum.intmpole_normal_avg[i])
+        return p
+
+    def get_intmpole_skew_avg(self, data_set, n):
+        """Return average integrated skew multipole."""
+        i = self.harmonics.index(n)
+        data = self._rotcoildata[data_set]
+        p = []
+        for datum in data:
+            p.append(datum.intmpole_skew_avg[i])
+        return p
 
     def get_files(self, data_set):
         """Return list of data files in a data set."""
@@ -137,7 +199,7 @@ class RotCoilMeas:
             self.serial_number + '/main'
         return data_path
 
-    def _get_data_set(self):
+    def _get_data_sets(self):
         data_path = self._get_data_path()
         files = os.listdir(data_path)
         return files
@@ -151,14 +213,17 @@ class RotCoilMeas:
     def _read_rotcoil_data(self):
         # read rotcoildata
         self._rotcoildata = dict()
-        for data_set in self.data_set:
-            dataset_datum = list()
+        for data_set in self.data_sets:
+            tstamps, mdata = [], []
             files = self.get_files(data_set)
             for file in files:
                 path = self._get_data_path()
                 path += '/' + data_set + '/' + file
                 meas = RotCoilData(path)
-                dataset_datum.append(meas)
+                tstamps.append(meas.hour)
+                mdata.append(meas)
+            # sort by timestamp
+            dataset_datum = [d for _, d in sorted(zip(tstamps, mdata))]
             self._rotcoildata[data_set] = dataset_datum
         # check consistency of meas data
         self._check_measdata()
@@ -184,6 +249,7 @@ class RotCoilMeas_SI(RotCoilMeas):
 class RotCoilMeas_Quad:
     """Rotation coil measurement of quadrupole magnets."""
 
+    main_harmonic = 2
     pass
 
 
@@ -192,14 +258,3 @@ class RotCoilMeas_SIQuadQ14(RotCoilMeas_SI, RotCoilMeas_Quad):
 
     magnet_type_label = 'Q14'
     magnet_type_name = 'si-quadrupoles-q14'
-
-
-m = RotCoilMeas_SIQuadQ14('002')
-# print(m.get_files('M1'))
-
-fn = ('/home/fac_files/lnls-ima/si-quadrupoles-q14/'
-      'rotating coil measurements/Q14-002/main/M1/'
-      'Q14-002_Q_BOA_006.0A_180409_172247.dat')
-# r = RotCoilData(fn)
-# txt = r.get_text().split()
-# print(len(txt))
