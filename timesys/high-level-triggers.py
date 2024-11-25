20d862c3ed04f4d004f21aa52f35e2fc55882d4b
Ana Clara Oliveira <ana.clara@lnls.br>
2024-11-25 18:08:09 -0300

    TI.MNT: group definition of new trigger with other BO-Glob:TI-LLRF triggers

--- timesys/high-level-triggers.py
+++ timesys/high-level-triggers.py
@@ -662,20 +662,6 @@
             'BO-49D:PS-CH:BCKPLN', 'BO-49U:PS-CV:BCKPLN',
             ),
         },
-    'BO-Glob:TI-LLRF-FDL': {
-        'database': {
-            'Src': {'enums': ()},
-            'Delay': {'value': 0.0},
-            'RFDelayType': {'value': 1, 'states': (2, 0)},
-            'NrPulses': {'value': 1},
-            'Duration': {'value': 1000},
-            'State': {'value': 0},
-            'Polarity': {'value': 0, 'states': (0, 1)},
-            },
-        'channels': (
-            'RA-RaBO01:RF-DigPatch:IN1',
-            ),
-        },
     'TB-Glob:TI-Mags': {
         'database': {
             'Src': {'enums': ('Cycle', )},
@@ -1088,6 +1074,20 @@
             'Polarity': {'value': 0, 'states': (0, 1)},
             },
         'channels': ('RA-RaBO01:RF-DigPatch:ET', ),
+        },
+    'BO-Glob:TI-LLRF-FDL': {
+        'database': {
+            'Src': {'enums': ()},
+            'Delay': {'value': 0.0},
+            'RFDelayType': {'value': 1, 'states': (2, 0)},
+            'NrPulses': {'value': 1},
+            'Duration': {'value': 1000},
+            'State': {'value': 0},
+            'Polarity': {'value': 0, 'states': (0, 1)},
+            },
+        'channels': (
+            'RA-RaBO01:RF-DigPatch:IN1',
+            ),
         },
     'SI-Glob:TI-LLRF-Rmp': {
         'database': {
