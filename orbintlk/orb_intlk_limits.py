3cddc2b5a2810a2321112a0da49a03f6d62c4680
Ana Clara Oliveira <ana.clara@lnls.br>
2025-07-04 18:13:23 -0300

    ENH: update orbit interlock data related to new 07SP VPU

--- orbintlk/orb_intlk_limits.py
+++ orbintlk/orb_intlk_limits.py
@@ -10,6 +10,7 @@
 
 {  # units in [um] and [urad]  # noqa: B018
     '06SB': {'psx': 600, 'psy': 600, 'agx': 300, 'agy': 300},  # VPU
+    '07SP': {'psx': 600, 'psy': 600, 'agx': 300, 'agy': 300},  # VPU
     '08SB': {'psx': 1000, 'psy': 1000, 'agx': 400, 'agy': 200},  # IVU's
     '10SB': {'psx': 500, 'psy': 500, 'agx': 400, 'agy': 400},  # Delta
     '14SB': {'psx': 1000, 'psy': 1000, 'agx': 400, 'agy': 200},  # IVU's
